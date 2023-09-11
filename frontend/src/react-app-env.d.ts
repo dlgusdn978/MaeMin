f3047f018a09d294ca2b098e978c42091eb5c363
권영재 <dwdjjj@naver.com>
2023-09-11 16:25:31 +0900

    Feat : Homepage init

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="react-scripts" />
+
+interface IconTypeProps {
+	width?: number;
+	height?: number;
+	color?: string;
+}
+
+type IconType = (props: IconTypeProps) => JSX.Element;
+
+interface IconProps {
+	iconSize?: number;
+	icon: IconType;
+}
+
+interface CardProps {
+	width?: number;
+	height?: number;
+	title?: string;
+	content?: string;
+}
