2c71ae6ad29355d8dddfcdb38afd76dce1b99150
이승태 <bs007bsk@naver.com>
2023-09-13 13:55:05 +0900

    [FE] Feat:트렌드 키워드 배치

--- frontend/src/components/TrendKeyword.tsx
+++ frontend/src/components/TrendKeyword.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 import TagIcon from '../assets/imgs/tag.svg';
+
+const TrendKeywordContainer = styled.div`
+	position: relative;
+	height: 52px;
+	min-width: 390px;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+	margin-top: 10px;
+`;
+
+const ContentContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+`;
+
+const TrendName = styled.div`
+	font-size: large;
+	position: relative;
+	margin-left: 10px;
+`;
+
 const TrendKeyword = () => {
 	return (
-		<div>
-			{/* 매장상세 2 */}
-			<img src={TagIcon} alt="Tag" /> <span>트렌드 키워드</span>
+		<TrendKeywordContainer>
+			<ContentContainer>
+				{/* 아이콘 */}
+				<img src={TagIcon} alt="Tag" />
+
+				{/* 트렌드 이름 */}
+				<TrendName>트렌드 키워드</TrendName>
+			</ContentContainer>
 			<div>트랜드 키워드 나열~</div>
-		</div>
+		</TrendKeywordContainer>
 	);
 };
 
