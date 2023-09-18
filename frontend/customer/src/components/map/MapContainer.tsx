a88b00eda7780bbdd2d24966f297430d6baa7d00
권영재 <dwdjjj@naver.com>
2023-09-18 17:56:32 +0900

    [FE] Feat : connect kakao map

---
+++ frontend/customer/src/components/map/MapContainer.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+
+const { kakao } = window;
+
+declare global {
+	interface Window {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		kakao: any;
+	}
+}
+
+const MapContainer = () => {
+	useEffect(() => {
+		const container = document.getElementById('map');
+		const options = {
+			center: new kakao.maps.LatLng(37.420125, 127.126665),
+			level: 3,
+		};
+
+		const map = new kakao.maps.Map(container, options);
+		console.log(map);
+	}, []);
+
+	return (
+		<div className="kakaomap">
+			<div id="map" style={{ width: '500px', height: '500px' }} />
+		</div>
+	);
+};
+
+export default MapContainer;
