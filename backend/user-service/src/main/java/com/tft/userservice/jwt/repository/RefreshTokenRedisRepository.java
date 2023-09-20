2277f12d5ed3e6aabc11418a41b1a0228bc16980
전윤철 <tlfldntm4444@naver.com>
2023-09-20 11:53:25 +0900

    [BE] Init : git dir 구조 변경 - card

--- backend/user-service/src/main/java/com/tft/userservice/jwt/repository/RefreshTokenRedisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tft.userservice.jwt.repository;
-
-import com.tft.userservice.jwt.redis.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, Long> {
-}
