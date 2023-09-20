d071318879aa1b2a20845d9a154d9a60b8759e8c
신관우 <89506035+Kwoo9@users.noreply.github.com>
2023-09-20 17:55:55 +0900

    test dsfsadf

--- frontend/customer/src/App.tsx
+++ frontend/customer/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MainRoutes } from './Router';
 import Layout from './components/layout/Layout';
-
 function App() {
 	return (
 		<Router>
