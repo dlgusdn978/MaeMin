365c4f686298567af386200c97ac6f6017f950ef
권영재 <dwdjjj@naver.com>
2023-09-25 13:58:54 +0900

    [FE] Chore : 401시 만료체크 로직추가필요

--- frontend/owner/src/api/base.ts
+++ frontend/owner/src/api/base.ts
@@ -8,18 +8,17 @@ axios.defaults.baseURL = 'https://j9c208.p.ssafy.io';
 axios.defaults.withCredentials = true;
 
 const accessToken = localStorage.getItem('access_token');
-const refreshToken = localStorage.getItem('refresh_token');
+const expiredTime = localStorage.getItem('expired_token');
 
 /** 1. 요청 전 - access토큰있는데 만료되면 refresh토큰도 헤더담아서 요청보내기 */
 API.interceptors.request.use(
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	(config: any) => {
 		if (accessToken) {
-			/** 2. access토큰 있으면 만료됐는지 체크 */
-			if (!accessToken) {
-				/** 3. 만료되면 만료된 access, refresh 같이 헤더 담아서 요청 */
+			/** 2. access토큰 있으면 만료 체크 로직 추가예정 */
+			if (!expiredTime) {
+				/** 3. 만료되면 만료된 access 헤더 담아서 요청 */
 				config.headers!.Authorization = `${accessToken}`;
-				config.headers!.Refresh = `${refreshToken}`;
 			} else {
 				config.headers!.Authorization = `${accessToken}`;
 			}
@@ -47,5 +46,7 @@ API.interceptors.response.use(
 		return Promise.reject(error);
 	},
 );
+
+// 요청 이후 401 - code가 expired이면 reissue요청
 
 export default API;
