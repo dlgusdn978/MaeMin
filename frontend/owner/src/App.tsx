41ee5b0368c8560b3001240dfc2a324f18fccb4c
권영재 <dwdjjj@naver.com>
2023-09-26 15:59:50 +0900

    [FE] Fix : add provider into App.tsx

--- frontend/owner/src/App.tsx
+++ frontend/owner/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MainRoutes } from './Router';
 import Header from './components/Header';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 function App() {
 	return (
 		<Router basename="/owner">
-			<Header />
-			<MainRoutes />
+			<Provider store={store}>
+				<Header />
+				<MainRoutes />
+			</Provider>
 		</Router>
 	);
 }
