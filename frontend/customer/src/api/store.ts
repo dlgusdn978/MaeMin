9bb1c7bd3ef30b5df2cbf5624671ff60e0d63133
이승태 <bs007bsk@naver.com>
2023-10-04 17:57:52 +0900

    [FE] Chore : pull

--- frontend/customer/src/api/store.ts
+++ frontend/customer/src/api/store.ts
@@ -1,8 +1,5 @@
 import API from './base';
 
-// export const getStoreDetail = (storeId: number) =>
-// 	API.get(`/store-service/customer/stores/${storeId}`).then((res) => res.data);
-
 export const getStoresByCategory = (category: number) =>
 	API.get(`/store-service/customer/stores?category=${category}`).then((res) => res.data);
 export const getStoreList = (areaName: string) => {
