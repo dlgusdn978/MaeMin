b4759e7a9e166b121be9336cef2f43172f63d1a4
이승태 <bs007bsk@naver.com>
2023-10-05 02:35:36 +0900

    [FE] Feat : 아이디 중복검사

--- frontend/customer/src/api/signup.ts
+++ frontend/customer/src/api/signup.ts
@@ -13,19 +13,11 @@ export const sendSms = async (phoneNumber: string) => {
 };
 
 export const verifySms = async (phoneNumber: string, checkNum: string) => {
-	console.log(phoneNumber);
 	try {
 		const response = await API.post('/user-service/sms/auth', {
 			to: phoneNumber,
 			checkNum: checkNum,
 		});
-		if (response.data.success) {
-			console.log('인증번호 확인 성공');
-		} else {
-			console.log(response.data);
-			console.log('인증번호 확인 실패');
-			console.log(response);
-		}
 		return response;
 	} catch (error) {
 		console.error('SMS 확인에 실패했습니다:', error);
@@ -33,13 +25,12 @@ export const verifySms = async (phoneNumber: string, checkNum: string) => {
 	}
 };
 
-//아이디 중복검사
 export const checkIdDuplicate = async (checkId: string) => {
 	try {
 		const response = await API.post('/user-service/users/check', {
 			checkId: checkId,
 		});
-		return response;
+		return response.data; // API 응답 구조에 따라 수정
 	} catch (error) {
 		console.error('아이디 중복 검사 실패:', error);
 		throw error;
