1fe9490b270ebb0e909935334f769ba817cdafbe
이승태 <bs007bsk@naver.com>
2023-10-06 12:53:01 +0900

    [FE] Chore : 마이페이지 수정

--- frontend/customer/src/pages/MyPage.tsx
+++ frontend/customer/src/pages/MyPage.tsx
@@ -3,21 +3,12 @@ import { Container } from '../components/layout/common';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { getMyLog } from '../api/user';
-import { MyPageHeader, UserInfoBox, MyOrderHistory, BackgroundImage, OrderText } from '../components/style/mypage';
-import ReceiptImage from '../assets/imgs/receipt.jpg';
-import styled from 'styled-components';
-
-const Font = styled.div`
-	font-size: 25px;
-	margin-bottom: 20px;
-	margin-top: 20px;
-	text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.5);
-	font-weight: 700;
-`;
+import { MyOrderHistory, MyPageHeader, UserInfoBox, Font } from '../components/style/mypage';
+import { CardContainer } from '../components/Card';
+// import ReceiptImage from '../assets/imgs/receipt.jpg';
 
 const MyPage = () => {
 	const [myLog, setMyLog] = useState([]);
-	const [isLoading, setIsLoading] = useState(true); // Add loading state
 	const userInfo = useSelector((state: RootState) => state.user);
 
 	console.log(userInfo);
@@ -25,14 +16,12 @@ const MyPage = () => {
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				await getMyLog().then((res) => {
+				getMyLog().then((res) => {
 					console.log(res.data);
 					setMyLog(res.data);
 				});
 			} catch (error) {
 				console.error('There was an error fetching the data:', error);
-			} finally {
-				setIsLoading(false); // End loading
 			}
 		};
 
@@ -47,33 +36,26 @@ const MyPage = () => {
 				<div>이름 : {userInfo.userName}</div>
 				<div>페이등록여부 : {userInfo.pay ? 'TFT 회원' : '페이 정보 없음'}</div>
 			</UserInfoBox>
-			<Font>내 결제 내역 </Font>
 			<MyOrderHistory>
-				<BackgroundImage src={ReceiptImage} alt="Receipt" />
-				{isLoading ? (
-					<OrderText>
-						<div>매장 이름 : Loading...</div>
-						<div>결제 수단 : Loading...</div>
-						<div>결제 금액 : Loading...</div>
-						<p>주문 일시 : Loading...</p>
-					</OrderText>
-				) : myLog.length > 0 ? (
-					myLog.map((item: MyOrder, i) => (
-						<OrderText key={i}>
+				<Font>내 결제 내역</Font>
+				{myLog?.map((item: MyOrder, i) => {
+					return (
+						<CardContainer key={i}>
 							<div>매장 이름 : {item.storeName}</div>
 							<div>결제 수단 : {item.paymentMethod}</div>
 							<div>결제 금액 : {item.totalPrice}</div>
 							<p>주문 일시 : {item.createdDate.toString()}</p>
-						</OrderText>
-					))
-				) : (
-					<OrderText>
-						<div>매장 이름 :</div>
-						<div>결제 수단 :</div>
-						<div>결제 금액 :</div>
-						<p>주문 일시 :</p>
-					</OrderText>
-				)}
+							<div>
+								주문 메뉴 :
+								<ul>
+									{item.menuList.map((menu, menuIndex) => (
+										<li key={menuIndex}>{menu}</li>
+									))}
+								</ul>
+							</div>
+						</CardContainer>
+					);
+				})}
 			</MyOrderHistory>
 		</Container>
 	);
